27e9659c88d0bdcfbbae4381de67afc9b2ecdd0b
Liandra Gutierrez <lia622@hotmail.com>
2025-04-26 17:51:12 -0400

    Code Cleanup

--- Landmarks/Landmarks/MapView.swift
+++ Landmarks/Landmarks/MapView.swift
@@ -14,9 +14,12 @@ struct MapView: View {
     }
     private var region: MKCoordinateRegion {
         MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            center: CLLocationCoordinate2D(
+                latitude: 34.011_286,
+                longitude: -116.166_868
+            ),
             span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-            )
+        )
     }
 }
 
